b7b483f2f3940fab4f700799d5b96b22e8681eb3
Landon Harvey <landonharvey5@gmail.com>
2019-01-28 13:28:07 -0600

    MY SQL Queries

---
+++ where_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+SELECT first_name, gender
+FROM employees
+WHERE first_name = 'Irena' AND NOT gender = 'F' OR first_name = 'Vidya' AND NOT gender = 'F' OR first_name ='Maya'
+AND NOT gender = 'F';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE ('E%') OR last_name LIKE ('%E');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE ('E%') AND last_name LIKE ('%E');
+
+SELECT first_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE ('%12-25');
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE ('%12-25') AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
+
