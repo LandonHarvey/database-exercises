b1ede5715c103ee13e301b5eeae3704ba8e12829
Landon Harvey <landonharvey5@gmail.com>
2019-01-28 13:59:31 -0600

    GROUP BY Functions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -23,7 +23,7 @@ SELECT first_name, last_name
 FROM employees
 WHERE birth_date LIKE ('%12-25');
 
-SELECT  datediff(now(), hire_date)
+SELECT  datediff(curdate(), hire_date)
 FROM employees
 WHERE birth_date LIKE ('%12-25') AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 ORDER BY birth_date, hire_date DESC;
